960248cdb9b4cf92c5e5448f7c6867543fdbd036
Mohammadmahdi <mahdialvansaz@gmail.com>
2022-11-16 13:28:59 +0330

    add sort functionality and loading indicator

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="px-5 tbl-sm:px-10 tbl-md:px-14">
-    <div class="flex flex-col tbl-sm:flex-row tbl-sm:justify-between gap-10">
+    <div
+      class="flex flex-col tbl-lg:flex-row items-center tbl-sm:justify-between gap-10"
+    >
       <div
         class="bg-white dark:bg-dark-blue mx-auto tbl-sm:mx-0 shadow-md flex items-center py-4 px-5 rounded-md gap-5 w-full max-w-md"
       >
@@ -15,33 +17,73 @@
         />
       </div>
 
-      <div class="relative text-very-very-dark-blue dark:text-white">
-        <div
-          class="cursor-pointer flex items-center justify-between py-3 px-6 bg-white dark:bg-dark-blue w-56 rounded-sm shadow-md"
-          @click="showDropDown = !showDropDown"
-        >
-          <span>{{
-            filterRegion === "All" ? "Filter by region" : filterRegion
-          }}</span
-          ><span class="material-symbols-outlined"> expand_more </span>
-        </div>
-        <ul
-          class="py-3 flex flex-col gap-1 absolute top-14 bg-white dark:bg-dark-blue w-56 rounded-sm shadow-md"
-          :class="showDropDown ? 'block' : 'hidden'"
-        >
-          <li
-            class="cursor-pointer px-6 hover:bg-gray-200"
-            v-for="region in regions"
-            :key="region"
-            @click="changeRegion(region)"
+      <div
+        class="w-full flex flex-col tbl-sm:flex-row tbl-lg:justify-end gap-5"
+      >
+        <div class="relative text-very-very-dark-blue dark:text-white">
+          <div
+            class="cursor-pointer flex items-center justify-between py-3 px-6 bg-white dark:bg-dark-blue w-48 rounded-sm shadow-md"
+            @click="showFilterDropDown = !showFilterDropDown"
           >
-            {{ region }}
-          </li>
-        </ul>
+            <span>{{
+              filterRegion === "All" ? "Filter by region" : filterRegion
+            }}</span
+            ><span class="material-symbols-outlined"> expand_more </span>
+          </div>
+          <ul
+            class="py-3 flex flex-col gap-1 absolute top-14 bg-white dark:bg-dark-blue w-48 rounded-sm shadow-md"
+            :class="showFilterDropDown ? 'block' : 'hidden'"
+          >
+            <li
+              class="cursor-pointer px-6 hover:bg-gray-200"
+              v-for="region in regions"
+              :key="region"
+              @click="changeRegion(region)"
+            >
+              {{ region }}
+            </li>
+          </ul>
+        </div>
+        <div class="relative text-very-very-dark-blue dark:text-white">
+          <div
+            class="cursor-pointer flex items-center justify-between py-3 px-6 bg-white dark:bg-dark-blue w-52 rounded-sm shadow-md"
+            @click="showSortDropDown = !showSortDropDown"
+          >
+            <span>{{ `Sort by ${sortBase}` }}</span
+            ><span class="material-symbols-outlined"> expand_more </span>
+          </div>
+          <ul
+            class="py-3 flex flex-col gap-1 absolute top-14 bg-white dark:bg-dark-blue w-52 rounded-sm shadow-md"
+            :class="showSortDropDown ? 'block' : 'hidden'"
+          >
+            <li
+              class="cursor-pointer px-6 hover:bg-gray-200"
+              v-for="sortBase in sortBases"
+              :key="sortBase"
+              @click="changeSort(sortBase)"
+            >
+              {{ sortBase }}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
 
-    <div class="flex flex-wrap gap-8 pt-10">
+    <div
+      v-if="countries.length === 0"
+      class="absolute top-1/2 text-4xl left-1/2 -translate-x-1/2 -translate-y-1/2"
+    >
+      <div
+        class="w-20 rounded-full animate-spin aspect-square border-t-8 border-b-8 border-solid"
+      ></div>
+    </div>
+    <div
+      v-else-if="filteredCountries.length === 0"
+      class="absolute top-1/2 text-4xl left-1/2 font-bold -translate-x-1/2 -translate-y-1/2"
+    >
+      No Match!
+    </div>
+    <div v-else class="flex flex-wrap gap-8 pt-10">
       <RouterLink
         class="w-9/12 max-w-vsm mx-auto"
         v-for="country in filteredCountries"
@@ -67,13 +109,18 @@ import SingleCountry from "../components/SingleCountry.vue"
 
 const countries = ref([])
 const searchQuery = ref("")
-const filterRegion = ref("All")
 
-const showDropDown = ref(false)
+const filterRegion = ref("All")
+const sortBase = ref("Name")
+
+const showFilterDropDown = ref(false)
+const showSortDropDown = ref(false)
 
 const regions = ["All", "Africa", "Americas", "Asia", "Europe", "Oceania"]
+const sortBases = ["Name", "Population"]
 
 const filteredCountries = computed(() => {
+  console.log("fiCou")
   let filteredCountries = []
 
   if (filterRegion.value === "All") {
@@ -81,6 +128,18 @@ const filteredCountries = computed(() => {
   } else {
     filteredCountries = countries.value.filter(
       (country) => country.region === filterRegion.value
+    )
+  }
+
+  if (sortBase.value === "Population") {
+    console.log("pop")
+    filteredCountries.sort((c1, c2) => {
+      return c2.population - c1.population
+    })
+  } else if (sortBase.value === "Name") {
+    console.log("coun name")
+    filteredCountries.sort((c1, c2) =>
+      c1.name > c2.name ? 1 : c1.name < c2.name ? -1 : 0
     )
   }
 
@@ -100,12 +159,24 @@ const filteredCountries = computed(() => {
 
   filteredCountries = [...exactMatches, ...partialMAtches]
 
+  // filteredCountries.sort((c1, c2) => c1.population - c2.population)
+  // filteredCountries = filteredCountries.sort(() => {
+  //   if (sortBase.value === "Unsorted") return filteredCountries
+  //   else if (sortBase.value === "Country Name") return fi
+  // })
+
   return [...new Set(filteredCountries)]
 })
 
 const changeRegion = (region) => {
   filterRegion.value = region
-  showDropDown.value = false
+  showFilterDropDown.value = false
+}
+
+const changeSort = (base) => {
+  sortBase.value = base
+  console.log(sortBase.value)
+  showSortDropDown.value = false
 }
 
 onMounted(() => {
